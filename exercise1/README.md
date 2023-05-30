d9a8b658b2ef5f0377f48811823e51b6ca7f7657
Camilo Griffa <camilogriffa@protonmail.com>
2023-05-30 19:25:52 -0300

    Entrega ejercicios tema 1

---
+++ exercise1/README.md
@@ -0,0 +1,1 @@
+Entrega ejercicios tema 1
---
+++ exercise1/src/App.java
@@ -0,0 +1,31 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        byte number1 = 1;
+        short number2 = 2;
+        int number3 = 3;
+        long number4 = 4;
+        float decimal1 = 4.9f;
+        double decimal2 = 9.99d;
+        char caracter1 = 'c';
+        boolean verdadero = true;
+        boolean falso = false;
+        String cadena1 = "Camilo";
+        String cadena2 = "Griffa";
+        Integer numero1 = 54;
+        Long numero2 = 2L;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(decimal1);
+        System.out.println(decimal2);
+        System.out.println(caracter1);
+        System.out.println(verdadero);
+        System.out.println(falso);
+        System.out.println(cadena1);
+        System.out.println(cadena2);
+        System.out.println(numero1);
+        System.out.println(numero2);
+    }
+}
